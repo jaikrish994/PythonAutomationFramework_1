e345c1f77cc3d8422c963f83bf0544581080dd35
prabhaharan.velu@gsihealth.com <prabhaharan.velu@gsihealth.com>
2019-10-22 17:30:45 +0530

    Adding allure-reports& demo folder

--- tests/login_test.py
+++ tests/login_test.py
@@ -24,7 +24,7 @@ class TestLogin():
             homepage.click_welcome()
             homepage.click_logout()
             x = driver.title
-            assert x == "abc"
+            assert x == "OrangeHRM"
         except AssertionError as error:
             print("Assertion Error Occured")
             print(error)
